3a5dfb6903feb2a52afe23519d08aa0f27602df3
Art Furrer <2823246+bonham@users.noreply.github.com>
2020-09-26 20:40:16 +0200

    page 2 + 3

---
+++ src/components/CustomNavBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-navbar toggleable="sm" type="dark" variant="info">
+      <b-navbar-brand to="/">Elevation</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/page2">Page1</b-nav-item>
+          <b-nav-item to="/page3">Page2</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+import {
+  BNavbar,
+  BNavbarBrand,
+  BNavbarToggle,
+  BNavbarNav,
+  BNavItem,
+  BCollapse
+} from 'bootstrap-vue'
+
+export default {
+  name: 'CustomNavBar',
+  components: {
+    BNavbar,
+    BNavbarBrand,
+    BNavbarToggle,
+    BNavbarNav,
+    BNavItem,
+    BCollapse
+  }
+}
+</script>
+
+<style>
+</style>
